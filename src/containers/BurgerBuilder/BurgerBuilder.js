0891b45949f4a4a3973c63e44892d50cd66b65a4
susiesko <susie.sko@gmail.com>
2020-12-02 19:52:04 -0800

    after lecture 129

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,11 +5,20 @@ import Aux from '../../hoc/Auxilary';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      cheese: 2,
+      meat: 2
+    }
+  };
+
   render() {
     return (
       <Aux>
-        <Burger/>
-        <div>Buidler Controls</div>
+        <Burger ingredients={this.state.ingredients}/>
+        <div>Builder Controls</div>
       </Aux>
     );
   }
