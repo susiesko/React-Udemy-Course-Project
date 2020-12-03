4d449059e2a2f5548a498ca7260df7cbba2ce9a3
susiesko <susie.sko@gmail.com>
2020-12-02 19:40:36 -0800

    after lecture 128

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import Aux from '../../hoc/Auxilary';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
   render() {
     return (
       <Aux>
-        <div>Burger</div>
+        <Burger/>
         <div>Buidler Controls</div>
       </Aux>
     );
