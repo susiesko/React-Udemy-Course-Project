96e4534f8c2e8a48caa0ead3bb619fec432e033a
Susie <susie.sko@gmail.com>
2020-12-03 19:18:32 -0800

    after lecture 130

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,12 +6,20 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = props => {
-  const transformedIngredients = Object.keys(props.ingredients)
+  // map by key array, then map by an array that is the size of quantity provided to ingredient.
+  // Array(props.ingredients[igKey]) = Array(ingredientQuantity), essentially.
+  let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
       return [...Array(props.ingredients[igKey])].map((_, i) => <BurgerIngredient key={igKey + i} type={igKey}/>);
-    });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
 
-    console.log(transformedIngredients);
+  if (transformedIngredients.length === 0){
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
+  console.log(transformedIngredients);
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top"/>
