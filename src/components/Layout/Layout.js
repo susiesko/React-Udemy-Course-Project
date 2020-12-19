f2a6b4b5870460b9928b67680b0acaed9691e054
susiesko <susie.sko@gmail.com>
2020-12-18 16:54:09 -0800

    after lecture 143

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,14 @@
 import React from 'react';
+
 import Aux from '../../hoc/Auxilary';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 import classes from './Layout.module.css';
 
 const Layout = (props) => (
   <Aux>
     <div>
+      <Toolbar/>
       Toolbar, SideDrawer, Backdrop
     </div>
     <main className={classes.Content}>
