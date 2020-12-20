5aa7487f1686e678f5ede85081405a87d6c77247
Susie <susie.sko@gmail.com>
2020-12-19 18:56:57 -0800

    after lecture 146

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Aux from '../../hoc/Auxilary';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 import classes from './Layout.module.css';
 
@@ -9,7 +10,8 @@ const Layout = (props) => (
   <Aux>
     <div>
       <Toolbar/>
-      Toolbar, SideDrawer, Backdrop
+      {/* Toolbar, SideDrawer, Backdrop */}
+      <SideDrawer/>
     </div>
     <main className={classes.Content}>
       {props.children}
