2a1b905ea6f61994b7ffc0f1044ae5a84dba6959
susiesko <susie.sko@gmail.com>
2020-12-22 12:34:17 -0800

    after lecture 148

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -11,7 +11,7 @@ const toolbar = (props) => (
     <div className={classes.Logo}>
       <Logo/>
     </div>    
-    <nav><NavigationItems/></nav>
+    <nav className={classes.DesktopOnly}><NavigationItems/></nav>
   </header>
 );
 
