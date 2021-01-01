4eafe512f61f69a99a282bcca84cb35ebeb02a41
susiesko <susie.sko@gmail.com>
2021-01-01 15:18:41 -0800

    after lecture 185

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -7,17 +7,23 @@ const withErrorHandler = ( WrappedComponent, axios ) => {
   return class extends Component {
     state = { 
       error: null
-    }
+    } 
 
     componentWillMount () { // course is older than newest version of react :)
-      axios.interceptors.request.use(req => {
+      this.reqInterceptor = axios.interceptors.request.use(req => {
         this.setState({ error: null });
         return req;
       });
 
-      axios.interceptors.response.use(res => res, error => {
+      this.resInterceptor = axios.interceptors.response.use(res => res, error => {
         this.setState({ error: error });
       });
+    }
+
+    componentWillUnmount() { // prevent memory leaks since we might be using this component in several places in the code base. 
+      console.log('Will Unmount', this.reqInterceptor, this.resInterceptor);
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.response.eject(this.resInterceptor);
     }
 
     errorConfirmedHandler = () => {
