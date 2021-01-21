1c2fbd85236139ecc45745f6c22d4d9b43097796
susiesko <susie.sko@gmail.com>
2021-01-21 12:14:38 -0800

    after lecture 279

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -13,7 +13,6 @@ import * as actionTypes from '../../store/actions';
 
 class BurgerBuilder extends Component {
   state = {
-    purchasable: false,
     purchasing: false,
     loading: false,
     error: null
@@ -34,7 +33,8 @@ class BurgerBuilder extends Component {
       sum += ingredients[idx];
     }
 
-    this.setState({ purchasable: sum > 0 });
+    return sum > 0;
+    //this.setState({ purchasable: sum > 0 });
   }
 
   // addIngredientHandler = (type) => {
@@ -118,7 +118,7 @@ class BurgerBuilder extends Component {
             ingredientRemoved={this.props.onIngredientRemoved}
             disabled={disabledInfo}
             price={this.props.price}
-            purchasable={this.state.purchasable}
+            purchasable={this.updatePurchaseState(this.props.ings)}
             ordered={this.purchaseHandler}
           />    
         </Aux>      
