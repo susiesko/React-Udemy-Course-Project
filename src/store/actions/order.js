1e8f1023d10236f0a02f40f8d1a870be7a209287
Susie <susie.sko@gmail.com>
2021-01-26 07:16:30 -0800

    after lecture 307

---
+++ src/store/actions/order.js
@@ -0,0 +1,26 @@
+import * as actionTypes from './actions';
+import axios from '../../axiosOrders';
+
+export const purchaseBurgerSuccess = (id, orderData) => ({
+  type: actionTypes.PURCHASE_BURGER_SUCCESS,
+  orderId: id,
+  orderData
+});
+
+export const purchaseBurgerFail = error => ({
+  type: actionTypes.PURCHASE_BURGER_FAIL,
+  error
+});
+
+export const purchaseBurgerStart = orderData => {
+  return dispatch => {    
+    axios.post('/orders.json', orderData)
+      .then(response => {
+        console.log(response.data);
+        dispatch(purchaseBurgerSuccess(response.data, orderData));
+      })
+      .catch(error => {
+        dispatch(purchaseBurgerFail( error ));
+      });
+  }  
+};
