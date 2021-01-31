ee6589139ec4f504904348ff998fb89ced12fa0e
Susie <susie.sko@gmail.com>
2021-01-30 19:21:55 -0800

    after lecture 327

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,5 @@
 import * as actionTypes from './actions';
+import axios from 'axios';
 
 export const authStart = () => ({
   type: actionTypes.AUTH_START
@@ -17,5 +18,19 @@ export const authFail = error => ({
 export const auth = (email, password) => {
   return dispatch => {
     dispatch(authStart());
+    const authData = {
+      email,
+      password,
+      returnSecureToken: true
+    };
+
+    axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCZ6_pcXO2_N-l8V-qZy9Odpia6Qiz6IUU', authData)
+      .then(res => {
+        console.log(res);
+        dispatch(authSuccess(res.data));
+      }).catch(err => {
+        console.error(err);
+        dispatch(authFail(err));
+      });
   }
 };
