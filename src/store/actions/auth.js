c01ccefabd1795f6861f92385eaab7678b19d7eb
Susie <susie.sko@gmail.com>
2021-01-31 18:27:30 -0800

    after lecture 336

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -48,6 +48,10 @@ export const auth = (email, password, isSignup) => {
 
     axios.post(url, authData)
       .then(res => {
+        const expirationDate = new Date(new Date().getTime() + res.data.expiresIn * 1000);
+        localStorage.setItem('token', res.data.idToken);
+        localStorage.setItem('expirationDate', expirationDate);
+        localStorage.setItem('userId', res.data.localId);
         dispatch(authSuccess(res.data.idToken, res.data.localId));
         dispatch(checkAuthTimeout(res.data.expiresIn));
       }).catch(err => {
@@ -57,10 +61,34 @@ export const auth = (email, password, isSignup) => {
 };
 
 export const logout = () => {
+  localStorage.removeItem('token');
+  localStorage.removeItem('expirationDate');
   return authLogout();
 }
 
 export const setAuthRedirectPath = path => ({
   type: actionTypes.SET_AUTH_REDIRECT_PATH,
   path
-})
+});
+
+export const authCheckState = () => {
+  return dispatch => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      dispatch(logout());
+    } else {
+      const expirationDate = new Date(localStorage.getItem('expirationDate'));
+
+      if (expirationDate > new Date()){
+        const userId = localStorage.getItem('userId');
+        dispatch(authSuccess(token, userId));
+        dispatch( 
+          checkAuthTimeout(
+            expirationDate.getSeconds() - new Date().getSeconds() )
+        );
+      } else {
+        dispatch(logout());
+      }
+    }
+  };
+}
