3ce72c4600f3e3eec71da7d2be30995e6eb23b0f
Susie <susie.sko@gmail.com>
2021-02-02 07:26:24 -0800

    after lecture 361

--- src/containers/BurgerBuilder/BurgerBuilder.test.js
+++ src/containers/BurgerBuilder/BurgerBuilder.test.js
@@ -18,6 +18,5 @@ describe('<BurgerBuilder/>', () => {
   it('should render <BuildControls/> when receiving ingredients', () => {
     wrapper.setProps({ings: { salad: 0 }});
     expect(wrapper.find(BuildControls)).toHaveLength(1);
-  })
-  
+  });  
 })
